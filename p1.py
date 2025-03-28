ab9512355f41cec21e2814b5fb0f045dfc8a74ec
nnassrull <nasrul2001@gmail.com>
2025-03-28 22:14:35 +0530

    Added activation function and spiral dataset

--- p1.py
+++ p1.py
@@ -1,6 +1,6 @@
 import numpy as np
 import nnfs
-
+from nnfs.datasets import spiral_data
 nnfs.init()
 
 np.random.seed(0)
@@ -9,6 +9,7 @@ X = [[   1,   2,   3,  2.5],
      [   2,   5,  -1,    2],
      [-1.5, 2.7, 3.3, -0.8]]
 
+X, y = spiral_data(100, 3)
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
@@ -22,12 +23,14 @@ class Activation_ReLu:
         self.output = np.maximum(0, inputs)
 
 
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
-
+layer1 = Layer_Dense(2, 5) # 5 number of neurons
+# layer2 = Layer_Dense(5, 2)
+activation1 = Activation_ReLu() # activation function
 layer1.forward(X)
-print(layer1.output)
+# print(layer1.output)
+activation1.forward(layer1.output)
+print(activation1.output)
 
-layer2.forward(layer1.output)
-print(layer2.output)
+# layer2.forward(layer1.output)
+# print(layer2.output)
 
