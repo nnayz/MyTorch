90031ac9b6dcbdaff37546d292b73cbecc5452a5
nnassrull <nasrul2001@gmail.com>
2025-03-28 17:09:23 +0530

    Added nnfs

--- p1.py
+++ p1.py
@@ -1,12 +1,33 @@
 import numpy as np
+import nnfs
 
-inputs = [ 1, 2, 3, 2.5 ]
-weights = [
-            [ 0.2,   0.8,  -0.5,   1.0],
-            [ 0.5,  -0.91, 0.26,  -0.5],
-            [-0.26, -0.27, 0.17,  0.87]
-        ]
-biases = [2, 3, 0.5]
+nnfs.init()
 
-output = np.dot(weights, inputs) + biases
-print(output)
+np.random.seed(0)
+
+X = [[   1,   2,   3,  2.5],
+     [   2,   5,  -1,    2],
+     [-1.5, 2.7, 3.3, -0.8]]
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLu:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
+
+layer1.forward(X)
+print(layer1.output)
+
+layer2.forward(layer1.output)
+print(layer2.output)
+
